11e1dfd22f09aa7c5e3f66475235f3c9c1e8bccc
SaraGraveline <graveline.sara@gmail.com>
2022-02-08 15:23:56 -0500

    Activity 5 - for now deleted the unnecessary comments

--- bubble-popper/js/script.js
+++ bubble-popper/js/script.js
@@ -1,33 +1,43 @@
 /**
-Title of Project
-Author Name
+Activity 5: Bubble Popper
+Sara Graveline
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+This activity is about playing around with ml5 and the feature of handpose. And also too get confortable with it.
+To do list:
+  - Get handpose working
+  - Bubble Popper
+  - Drawing a pin
+  - Movement of Bubble
 */
 
 "use strict";
 
+//Access the users webcam
+let video = undefined;
 
-/**
-Description of preload
-*/
+//Access the handpose
+let handpose = undefined;
+
+
 function preload() {
 
 }
 
-
-/**
-Description of setup
-*/
 function setup() {
+  createCanvas(640, 480);
 
+  //This access the user's webcam
+  video = createCapture(VIDEO);
+  video.hide();
+
+  //Loads the handpose model
+  handpose = ml5.handpose(video, {
+    flipHorizontal: true
+  },  function() {
+    console.log(`Model loaded.`);
+  });
 }
 
-
-/**
-Description of draw()
-*/
 function draw() {
 
 }
