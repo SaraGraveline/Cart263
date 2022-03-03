50b6f8fe3a5bf20814be45c122e38af1af6a3da5
SaraGraveline <graveline.sara@gmail.com>
2022-03-02 23:48:40 -0500

    P1 - restarted the whole project and endings some intro scenes with one challenge

--- Project 1/js/script.js
+++ Project 1/js/script.js
@@ -2,134 +2,255 @@
 Project 1 - A Night at the Movies
 Sara Graveline
 
-Some ideas:
-- minions movie
-  - minions trying to find other villains to connent with
-  - like moving from one side of the screen sqaure, it starts a small game which ends in bad and then the minions comes back to the main page and they die again.
-- One eposide of run BTS
-- game of throne where the user will have to climb the wall
-- up movie where the user has to add and pop some ballons to go up and down
-- Despicale me where the guy is trying to enter into the villan's house so the idea is to not let that happen.
-- emotion game where you need to find good emotions to change the negative emotions
-- tron game where you grab the disk and bring it to the other side and make a goal however there will be some obstacles.
-- Nerve movies - based on texting - someone asks a question and you reply and  it tells you next steps.
-  - and depending on the dares you get to do some those things. Like different games:
-    - 1) like if the dare is to say something bad you did that you never told anyone and the player has to turn on their microphone to say it out loud
-    - so on
+Movie Nerve
 **/
 
 "use strict";
 
-// Current state of program
-let state = `loading`; // loading, running
-// User's webcam
-let video;
-// The name of our model
-let modelName = `Handpose`;
-// Handpose object (using the name of the model for clarity)
-let handpose;
-// The current set of predictions made by Handpose once it's running
-let predictions = [];
+let state = 0;
 
-/**
-Starts the webcam and the Handpose
-*/
+let vid;
+let shot;
+let eye;
+let file;
+let skull;
+let fingerprint;
+let timer;
+let hacker;
+let goodluck;
+
+let userData = {
+  name: `stranger`
+};
+
+function preload() {
+  vid = loadImage('assets/player.gif');
+  shot = loadImage(`assets/shoting.gif`);
+  eye = loadImage( `assets/eye.gif`);
+  file = loadImage(`assets/files.gif`);
+  skull = loadImage(`assets/skull.gif`);
+  fingerprint = loadImage(`assets/fingerprint.gif`);
+  timer = loadImage(`assets/timer.gif`);
+  hacker = loadImage(`assets/hacker.gif`);
+  goodluck = loadImage(`assets/goodluck.gif`);
+};
+
 function setup() {
-  createCanvas(640, 480);
+  createCanvas(windowWidth, windowHeight);
+};
 
-  // Start webcam and hide the resulting HTML element
-  video = createCapture(VIDEO);
-  video.hide();
-
-  // Start the Handpose model and switch to our running state when it loads
-  handpose = ml5.handpose(video, {
-    flipHorizontal: true
-  }, function() {
-    // Switch to the running state
-    state = `running`;
-  });
-
-  // Listen for prediction events from Handpose and store the results in our
-  // predictions array when they occur
-  handpose.on(`predict`, function(results) {
-    predictions = results;
-  });
-}
-
-/**
-Handles the two states of the program: loading, running
-*/
 function draw() {
-  if (state === `loading`) {
-    loading();
-  }
-  else if (state === `running`) {
-    running();
-  }
-}
+  //if statement for the state variable. This changes the screens from start, middle or to end.
+  if(state == 0) {
+    startScreen()
+  }else if (state == 1) {
+    intro()
+  }else if (state == 2) {
+    endintro()
+  }else if (state == 3) {
+    startGame()
+  } else if (state == 4) {
+    challengeOne()
+  } else if (state == 5) {
+    challengeOneEnd()
+  } else if (state == 6) {
+    challengeTwo()
+  };
+};
 
-/**
-Displays a simple loading screen with the loading model's name
-*/
-function loading() {
-  background(255);
-
-  push();
-  textSize(32);
+//This function is for the start screen/ intro screen.
+function startScreen(){
+  background(0);
+  textAlign(CENTER);
+  textSize(60);
+  fill(255, 68, 204)
+  textFont(`gasalt`);
   textStyle(BOLD);
-  textAlign(CENTER, CENTER);
-  text(`Loading ${modelName}...`, width / 2, height / 2);
-  pop();
-}
 
-/**
-Displays the webcam.
-If there is a hand it outlines it and highlights the tip of the index finger
-*/
-function running() {
-  // Display the webcam with reveresd image so it's a mirror
-  let flippedVideo = ml5.flipImage(video);
-  image(flippedVideo, 0, 0, width, height);
-
-  // Check if there currently predictions to display
-  if (predictions.length > 0) {
-    // Technically there will only be ONE because it only detects ONE hand
-    // Get the hand predicted
-    let hand = predictions[0];
-    // Highlight it on the canvas
-    highlightHand(hand);
+  if (keyIsPressed) {
+    stroke(255);
+    strokeWeight(4);
   }
-}
-
-/**
-Provided with a detected hand it highlights the tip of the index finger
-*/
-function highlightHand(hand) {
-  // Display a circle at the tip of the index finger
-  let index = hand.annotations.indexFinger[3];
-  let indexX = index[0];
-  let indexY = index[1];
-
-  let thumb = hand.annotations.thumb[3];
-  let thumbX = thumb[0];
-  let thumbY = thumb[1];
-
-  let d = dist(indexX, indexY, thumbX, thumbY);
-  if (d < 20) {
-    fill(0, 255, 0);
-  } else {
-    fill(255, 0, 0);
+  else {
+    noStroke();
   }
 
   push();
-  fill(255, 255, 0);
-  noStroke();
-  ellipse(indexX, indexY, 20);
+  fill(0, 242, 222)
+  textSize(200);
+  text('NERVE', width/2, height/2);
   pop();
 
   push();
-  fill(255, 255, 0);
-  noStroke();
-  ellipse(thumbX, thumbY, 20);
+  text('WATCHER OR', width/2-100, height/2+50);
   pop();
+
+  push();
+  text('PLAYER', width/2+160, height/2+50);
+  pop();
+
+  push();
+  stroke(255);
+  strokeWeight(2);
+  line(950, 497, 635, 497);
+  pop();
+};
+
+function intro(){
+  background(0);
+  image(vid, 0,0);
+  image(shot, 100, 600, 700, 400);
+  image(eye, 1400, 600, 300, 300);
+  image(file, 900, 0, 900, 400);
+  image(skull, 450, 250);
+  image(fingerprint, 950, 450);
+  image(timer, 600, 50, 100, 100);
+  image(hacker, 70, 280, 250, 250);
+}
+
+function endintro(){
+  background(0); //background image
+  fill(0, 242, 222);
+  textFont(`gasalt`);
+  textSize(30);
+  text('Press any key to start', width/2, 800);
+  image(goodluck, 500,-40, 800, 800);
+};
+
+function startGame() {
+  push();
+  challengeCompleted();
+  text(`Are you ready for your first challenge!`, width/2, height/4);
+  pop();
+
+  push();
+  fill(255);
+  rect(width/2.4, height/2, 300, 120, 40);
+  acceptButton();
+  text(`1 to ACCEPT`, width/2, height/2+70)
+  pop();
+
+  emojis();
+
+  if(key ===`1`) {
+    state = 4
+  };
+};
+
+function challengeOne() {
+  let f = createCanvas(windowWidth, windowHeight);
+ // Add an event for when a file is dropped onto the canvas
+ f.drop(kidPhoto);
+
+ push();
+ challengeTypography();
+ text('Challenge 1 -', width/2, height/2 - 100)
+ text('Drag and Drop a image of your child onto this canvas.', width / 2, height / 2);
+ pop();
+};
+
+function challengeOneEnd() {
+  push();
+  challengeCompleted();
+  text(`  Well Done!
+    "🎊" You recieved $300. "🎉",
+    Next Challenge`, width/2, height/4);
+  pop();
+
+  push();
+  fill(255);
+  rect(width/2.4, height/2+100, 300, 120, 40);
+  acceptButton();
+  text(`2 to Accept`, width/2, height/2+170)
+  pop();
+
+  emojis();
+
+  if(key ===`2`) {
+    state = 6
+  };
+};
+
+function challengeTwo() {
+  push();
+  challengeTypography();
+  text('Challenge 2 -', width/2, height/2 - 300);
+  text('Rob a bank by finding the bank code.', width / 2, height/2 - 200);
+  pop();
+
+  push();
+  fill(0);
+  textSize(20);
+  text(`hint: console`, width/2, height/2+350);
+  pop();
+
+  let data =  JSON.parse(localStorage.getItem(`web-storage-example-personalization`));
+  if (data !== null) {
+    userData.name = data.name;
+  }
+  else {
+    userData.name = prompt(`What is your SIN number?`);
+    localStorage.setItem(`web-storage-example-personalization`, JSON.stringify(userData));
+  }
+
+  display();
+};
+
+function display(){
+  textSize(64);
+  textAlign(CENTER);
+  text(`Hello, Pippin!`, width/2, height/2);
+}
+
+function mousePressed (){
+  if(state == 0){
+    state = 1
+} else if(state == 1) {
+    state = 2
+  } else if (state == 2) {
+    state = 3
+  };
+};
+
+function emojis() {
+  //by cassie titled hacker on p5js
+  push();
+  textSize(50);
+  let emojis = ["🎉", "🎊", "🥳", "💖", "🌈","🤑", "💸", "💰", "💵"];
+  let emoji = emojis[floor(random(0, emojis.length))];
+  text(emoji, random(0, width), random(0, height));
+  pop();
+};
+
+//https://p5js.org/reference/#/p5.Element/drop
+function kidPhoto(file) {
+  // If it's an image file
+  if (file.type === 'image') {
+    // Create an image DOM element but don't show it
+    let kidphoto = createImg(file.data).hide();
+    // Draw the image onto the canvas
+    image(kidphoto, 0, 0, width, height);
+  }; state = 5;
+};
+
+function challengeTypography() {
+  background(255, 68, 204);
+  fill(255);
+  noStroke();
+  textSize(80);
+  textAlign(CENTER);
+};
+
+function challengeCompleted() {
+  background(0, 242, 222);
+  fill(255, 68, 204);
+  textFont(`gasalt`);
+  textSize(100);
+};
+
+function acceptButton() {
+  fill(255, 68, 204);
+  textSize(50);
+  textStyle(BOLD);
+  textFont(`gasalt`);
 };
