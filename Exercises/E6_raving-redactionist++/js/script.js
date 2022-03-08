960bda99c0fc5c8ae4ab8092b80d76c70f6db95b
SaraGraveline <graveline.sara@gmail.com>
2022-03-08 17:06:48 -0500

    E6: added the revelation function for the text to reveal randomly

--- Exercises/E6_raving-redactionist++/js/script.js
+++ Exercises/E6_raving-redactionist++/js/script.js
@@ -6,4 +6,20 @@ This codes a webpage of text with some passage redacted. And the redacted text a
 
 "use strict";
 
-// Code goes here
+//telling the revelation function.
+setInterval(revelation, 500);
+
+function revelation() {
+  $(`.redacted`).each(attemptReveal);
+};
+
+//reveal text
+function attemptReveal() {
+  let r = Math.random();
+  if (r < 0.1) {
+    $(this).removeClass(`redacted`);
+    $(this).addClass(`revealed`);
+  }
+};
+
+//hides the text
