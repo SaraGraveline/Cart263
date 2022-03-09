f7de517e0a570b66218b9e79db6e1c9543d4d164
SaraGraveline <graveline.sara@gmail.com>
2022-03-09 05:31:32 -0500

    E6: added the fade in and out on h1 and added comments

--- Exercises/E6_raving-redactionist++/js/script.js
+++ Exercises/E6_raving-redactionist++/js/script.js
@@ -1,26 +1,38 @@
 /**
 E6: Raving Redactionist
 
-This codes a webpage of text with some passage redacted. And the redacted text appears randoml.
+This codes a webpage of text with some passage redacted. And the redacted text appears randomly.
+The main goal is to try to hide the redacted text that is being revealy randomly.
 */
 
 "use strict";
+
+
+//makes tge h1 disappear and appear over and over with mouse click on top-secret class.
+$( ".top-secret" ).on(`click`, function(event) {
+  $( "h2" ).fadeOut(500, function() {
+    $(`h2`).fadeIn(500);
+  });
+});
+
+//enables the mouse click for the text to redact.
 $(`.top-secret`).on(`click`, redact);
-//telling the revelation function.
+//calls the revelation function and try to reveal the secret.
 setInterval(revelation, 500);
 
 
-//hides the text on click
+//hides the text that was revealed with a mouse click over it.
 function redact(event) {
   $(this).removeClass(`revealed`);
   $(this).addClass(`redacted`);
 };
 
+//calls each element from html with redacted class.
 function revelation() {
   $(`.redacted`).each(attemptReveal);
 };
 
-//reveal text
+//reveal the redacted text randomly with removeclass and reveals it with add class.
 function attemptReveal() {
   let r = Math.random();
   if (r < 0.1) {
@@ -28,3 +40,5 @@ function attemptReveal() {
     $(this).addClass(`revealed`);
   }
 };
+
+//thank you
