f97d99a16013d2ea5b26a774fe9af445c0b628d6
SaraGraveline <graveline.sara@gmail.com>
2022-03-15 15:05:41 -0400

    P2: trying to add my field in this game

--- Project 2- anything/js/script.js
+++ Project 2- anything/js/script.js
@@ -3,11 +3,15 @@ Project 2 - Anything
 Sara Graveline
 
 Ideas for this project:
-1. A music video where there is a list of songs to pick from and after selecting the lyrics to the song appears and you sing it with your microphone on.
+1. A music video where there is a list of songs to pick from and
+   after selecting the lyrics to the song appears and you sing it with your microphone on.
 2. A ecapse room inspired by BTS.
 3. A website where you create a collage by dragging and dropping stuff.
 4. The whole process of graphic design where you talk with the client, design the stuff and print it.
 5. escape room with graphic design.
+6. save the earth where you clean the ocean by picking up plastic, clean the air with your hand etc.
+7. guess the right person and show
+8. Type experiment where you create your own create your own type.
 */
 
 "use strict";
