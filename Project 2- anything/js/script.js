a6da66d14a62dbb17accb42c04ac92e5a98fdd0f
SaraGraveline <graveline.sara@gmail.com>
2022-03-15 14:57:53 -0400

    P2: top five ideas

--- Project 2- anything/js/script.js
+++ Project 2- anything/js/script.js
@@ -3,7 +3,11 @@ Project 2 - Anything
 Sara Graveline
 
 Ideas for this project:
-1. 
+1. A music video where there is a list of songs to pick from and after selecting the lyrics to the song appears and you sing it with your microphone on.
+2. A ecapse room inspired by BTS.
+3. A website where you create a collage by dragging and dropping stuff.
+4. The whole process of graphic design where you talk with the client, design the stuff and print it.
+5. escape room with graphic design.
 */
 
 "use strict";
