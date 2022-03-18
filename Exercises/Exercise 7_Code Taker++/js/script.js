84768b808f408cfeae0472991070c1e41f6b0da9
SaraGraveline <graveline.sara@gmail.com>
2022-03-18 19:53:15 -0400

    E7: added the mouseover to reveal letter and ui draggable to the box

--- Exercises/Exercise 7_Code Taker++/js/script.js
+++ Exercises/Exercise 7_Code Taker++/js/script.js
@@ -6,3 +6,19 @@ after discovering all the letters, the user will have to write it in the box.
 */
 
 "use strict";
+
+$(`.secret`).one(`mouseover`, function(event) {
+  $(this).addClass(`found`, 500);
+  $(this).draggable({
+    helper: `clone`
+  });
+});
+
+$(`#answer`).droppable({
+  drop: function(event, ui){
+    let letter = ui.draggable.text();
+    $(this).append(letter);
+    ui.draggable.draggable(`disable`);
+    ui.draggable.removeClass(`found`);
+  }
+});
