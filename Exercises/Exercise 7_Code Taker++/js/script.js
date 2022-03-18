3c5d3204b03344cbb63e8bd218a21ba4d6a9d70e
SaraGraveline <graveline.sara@gmail.com>
2022-03-18 18:48:33 -0400

    E7: deleted the previous code and added the title and description to it

--- Exercises/Exercise 7_Code Taker++/js/script.js
+++ Exercises/Exercise 7_Code Taker++/js/script.js
@@ -1,44 +1,8 @@
 /**
-E6: Raving Redactionist
+E7: Code Taker++
 
-This codes a webpage of text with some passage redacted. And the redacted text appears randomly.
-The main goal is to try to hide the redacted text that is being revealy randomly.
+This codes a webpage with hidden letters which is revealed when the user hovers on it and
+after discovering all the letters, the user will have to write it in the box.
 */
 
 "use strict";
-
-
-//makes tge h1 disappear and appear over and over with mouse click on top-secret class.
-$( `.top-secret` ).on(`click`, function(event) {
-  $( `h1`).fadeOut(500, function() {
-    $(`h1`).fadeIn(500);
-  });
-});
-
-//enables the mouse click for the text to redact.
-$(`.top-secret`).on(`click`, redact);
-//calls the revelation function and try to reveal the secret.
-setInterval(revelation, 500);
-
-
-//hides the text that was revealed with a mouse click over it.
-function redact(event) {
-  $(this).removeClass(`revealed`);
-  $(this).addClass(`redacted`);
-};
-
-//calls each element from html with redacted class.
-function revelation() {
-  $(`.redacted`).each(attemptReveal);
-};
-
-//reveal the redacted text randomly with removeclass and reveals it with add class.
-function attemptReveal() {
-  let r = Math.random();
-  if (r < 0.1) {
-    $(this).removeClass(`redacted`);
-    $(this).addClass(`revealed`);
-  }
-};
-
-//thank you
