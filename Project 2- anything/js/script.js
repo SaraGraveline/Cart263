743e4ef23ac57135162a5d75e894d19ea303733a
SaraGraveline <graveline.sara@gmail.com>
2022-03-19 05:57:16 -0400

    P2P1: added the droppable and draggablefunctions

--- Project 2- anything/js/script.js
+++ Project 2- anything/js/script.js
@@ -37,5 +37,51 @@ Game names brainstorm:
 */
 
 "use strict";
+//the answer dialog from html
+$(`#solved-dialog`).dialog({
+  autoOpen: false, //prevents the pop-up to open before the solution
+  //button so the user can close the pop-up
+  buttons: {
+    "I know!": function() {
+      $(this).dialog(`close`);
+    }
+  }
+});
 
-// Code goes here
+//this pop-up opens when the user answer it wrong
+$(`#failed-dialog`).dialog({
+  autoOpen: false,
+  buttons: {
+    "Nooooooo!! One more try!": function() {
+      $(this).dialog(`close`);
+    }
+  }
+});
+
+//Mouseover event which reveals the letters in red and also makes clone of the letter.
+$(`.secret`).one(`mouseover`, function(event) {
+  $(this).addClass(`found`, 500);
+  $(this).draggable({
+    helper: `clone`
+  });
+});
+
+//Makes the letters drappable and draggable
+$(`#answer`).droppable({
+  drop: function(event, ui){
+    let letter = ui.draggable.text();
+    $(this).append(letter); //when added to the red border box.
+    ui.draggable.draggable(`disable`); //after it is not draggable
+    ui.draggable.removeClass(`found`); //and the found class is removes after the task is done.
+    //if statement for when the answer is right and the pop-up opens up
+    if($(this).text() ===`🤗bangtan - sonyeondan`) {
+      $(`#solved-dialog`).dialog(`open`);
+    }
+    //if statement for when the answer is wrong and pop-up opens up
+    if($(this).text() === `bangtan - sonyeondan`) {
+      $(`#failed-dialog`).dialog(`open`);
+    }
+  }
+});
+
+// thank you.
