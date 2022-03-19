d082b8a312b33e806baf8b5c2ff7ff6be8a772d1
SaraGraveline <graveline.sara@gmail.com>
2022-03-19 05:55:14 -0400

    E7: added the sources

--- Exercises/Exercise 7_Code Taker++/js/script.js
+++ Exercises/Exercise 7_Code Taker++/js/script.js
@@ -3,6 +3,10 @@ E7: Code Taker++
 
 This codes a webpage with hidden letters which is revealed when the user hovers on it and
 after discovering all the letters, the user will have to write it in the box.
+
+images and gif sources:
+https://www.freepik.com/ // all animal images comes from this website.
+https://tenor.com/search/bts-gifs // all the gifs comes from this website.
 */
 
 "use strict";
