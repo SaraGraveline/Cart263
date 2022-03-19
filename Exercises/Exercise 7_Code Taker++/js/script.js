798f69de47b0536d0521f6b20a4cc54e6501d236
SaraGraveline <graveline.sara@gmail.com>
2022-03-19 05:46:23 -0400

    E7: added comments

--- Exercises/Exercise 7_Code Taker++/js/script.js
+++ Exercises/Exercise 7_Code Taker++/js/script.js
@@ -7,8 +7,10 @@ after discovering all the letters, the user will have to write it in the box.
 
 "use strict";
 
+//the answer dialog from html
 $(`#solved-dialog`).dialog({
-  autoOpen: false,
+  autoOpen: false, //prevents the pop-up to open before the solution
+  //button so the user can close the pop-up
   buttons: {
     "I know!": function() {
       $(this).dialog(`close`);
@@ -16,6 +18,7 @@ $(`#solved-dialog`).dialog({
   }
 });
 
+//this pop-up opens when the user answer it wrong
 $(`#failed-dialog`).dialog({
   autoOpen: false,
   buttons: {
@@ -25,6 +28,7 @@ $(`#failed-dialog`).dialog({
   }
 });
 
+//Mouseover event which reveals the letters in red and also makes clone of the letter.
 $(`.secret`).one(`mouseover`, function(event) {
   $(this).addClass(`found`, 500);
   $(this).draggable({
@@ -32,17 +36,22 @@ $(`.secret`).one(`mouseover`, function(event) {
   });
 });
 
+//Makes the letters drappable and draggable
 $(`#answer`).droppable({
   drop: function(event, ui){
     let letter = ui.draggable.text();
-    $(this).append(letter);
-    ui.draggable.draggable(`disable`);
-    ui.draggable.removeClass(`found`);
+    $(this).append(letter); //when added to the red border box.
+    ui.draggable.draggable(`disable`); //after it is not draggable
+    ui.draggable.removeClass(`found`); //and the found class is removes after the task is done.
+    //if statement for when the answer is right and the pop-up opens up
     if($(this).text() ===`🤗bangtan - sonyeondan`) {
       $(`#solved-dialog`).dialog(`open`);
     }
+    //if statement for when the answer is wrong and pop-up opens up
     if($(this).text() === `bangtan - sonyeondan`) {
       $(`#failed-dialog`).dialog(`open`);
     }
   }
 });
+
+// thank you.
