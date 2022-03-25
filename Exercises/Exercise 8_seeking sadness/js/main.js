340187169901bd02219cb04c6d04f133577d16d5
SaraGraveline <graveline.sara@gmail.com>
2022-03-25 19:17:11 -0400

    E8: added the game and config varbile to main

--- Exercises/Exercise 8_seeking sadness/js/main.js
+++ Exercises/Exercise 8_seeking sadness/js/main.js
@@ -2,9 +2,19 @@
 Desperately Seeking Sadness
 Sara Graveline
 
-This game is trying to find a emoji that is different from the other emoji. It tries to seek unhappiness because its life is way to happy. 
+This game is trying to find a emoji that is different from the other emoji. It tries to seek unhappiness because its life is way to happy.
 */
 
 "use strict";
 
-// Code goes here
+let config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: `arcade`
+  },
+  scene: [Boot, Play]
+};
+
+let game = new Phaser.Game(config);
