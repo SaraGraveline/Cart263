183952f158dd224c71aada3f0e4bfe458c2f37a6
SaraGraveline <graveline.sara@gmail.com>
2022-03-25 19:10:21 -0400

    E8: added preload, create and update to boot

---
+++ Exercises/Exercise 8_seeking sadness/js/Boot.js
@@ -0,0 +1,23 @@
+class Boot extends Phaser.Scene {
+  constructor() {
+    super ({
+      key: `boot`
+    })
+  }
+
+  preload() {
+    //load assets here!
+
+    this.load.on(`complete`, () => {
+      this.scene.start(`play`);
+    });
+  }
+
+  create() {
+
+  }
+
+  update() {
+
+  }
+}
