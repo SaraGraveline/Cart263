cec9e51e87309c57d8dbb4a4a503b83f9684ace9
SaraGraveline <graveline.sara@gmail.com>
2022-03-26 01:59:16 -0400

    E8: added the arrow key movement

--- Exercises/Exercise 8_seeking sadness/js/Play.js
+++ Exercises/Exercise 8_seeking sadness/js/Play.js
@@ -8,9 +8,24 @@ class Play extends Phaser.Scene {
   create() {
     //create the groom
     this.groom = this.physics.add.sprite(400, 300, `groom`);
+    //this prevents it from leaving the canvas
+    this.groom.setCollideWorldBounds(true);
+    //make the arrow key move the groom on the canvas.
+    this.cursors = this.input.keyboard.createCursorKeys();
   }
 
   update() {
-
+    if (this.cursors.left.isDown) {
+      this.groom.setVelocityX(-100);
+    }
+    if (this.cursors.right.isDown) {
+      this.groom.setVelocityX(100);
+    }
+    if (this.cursors.up.isDown) {
+      this.groom.setVelocityY(-100);
+    }
+    if (this.cursors.down.isDown) {
+      this.groom.setVelocityY(100);
+    }
   }
-}
+};
