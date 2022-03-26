6de46f5216ae92eeb6753af56dd52d30fe76e4a4
SaraGraveline <graveline.sara@gmail.com>
2022-03-26 03:49:03 -0400

    E8: added style to css

--- Exercises/Exercise 8_seeking sadness/js/main.js
+++ Exercises/Exercise 8_seeking sadness/js/main.js
@@ -4,15 +4,17 @@ Sara Graveline
 
 This game is trying to find a emoji that is different from the other emoji. It tries to seek unhappiness because its life is way to happy.
 
+https://www.flaticon.com/free-icon/bride_703218?term=bride&page=1&position=36
+https://www.flaticon.com/free-icon/groom_703219?related_id=703219&origin=search
+https://www.flaticon.com/premium-icon/zombie_2274555?term=zombie&page=1&position=57&page=1&position=57&related_id=2274555&origin=search
 https://freesound.org/people/plumaudio/sounds/488227/
-https://pngtree.com/freebackground/fresh-wedding-season-church-background-cartoon-design_954826.htmlHAPP
 */
 
 "use strict";
 
 let config = {
   type: Phaser.AUTO,
-  width: 1000,
+  width: 1200,
   height: 800,
   physics: {
     default: `arcade`
